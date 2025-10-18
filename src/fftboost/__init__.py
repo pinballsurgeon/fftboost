229fab83a8183b046b5fa9003f71809bc1221791
Dan Ehlers <pinballsurgeon@gmail.com>
2025-10-18 09:55:04 -0400

    dehls - fftboost classifier generalizations

--- src/fftboost/__init__.py
+++ src/fftboost/__init__.py
@@ -2,7 +2,15 @@ __version__ = "1.0.0"
 
 from .api import FFTBoost
 from .api import FFTBoostClassifier
+from .automl import AutoMLConfig
+from .automl import AutoMLController
 from .booster import BoosterConfig
 
 
-__all__ = ["FFTBoost", "FFTBoostClassifier", "BoosterConfig"]
+__all__ = [
+    "FFTBoost",
+    "FFTBoostClassifier",
+    "BoosterConfig",
+    "AutoMLController",
+    "AutoMLConfig",
+]
